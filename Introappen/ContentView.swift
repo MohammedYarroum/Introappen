77298b4e06e01745be9a1211ff679b1a01cdcc79
Mohammed Yarroum <101400003+MohammedYarroum@users.noreply.github.com>
2023-10-13 19:07:23 +0200

    Update ContentView.swift

--- Introappen/ContentView.swift
+++ Introappen/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hej, världen!")
+            Text("Hej, klas!")
                 .font(.title)
                 
         }
