0466f493d715b15e0457d2bd647abeb267e0c6d5
Mohammed <mohammed@yarroum.design>
2023-10-13 16:30:04 +0200

    Initial Commit

---
+++ Introappen/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Introappen
+//
+//  Created by Mohammed Yarroum on 2023-10-13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Introappen/IntroappenApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntroappenApp.swift
+//  Introappen
+//
+//  Created by Mohammed Yarroum on 2023-10-13.
+//
+
+import SwiftUI
+
+@main
+struct IntroappenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
